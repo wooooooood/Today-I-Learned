0f10b839ef5a9da4de4a7321d86c4d2899a8e4c6
wooooooood <40855076+wooooooood@users.noreply.github.com>
2021-07-23 11:54:15 +0900

    ASP.NET import excel file

---
+++ ASP.NET/import-excel.cshtml.cs
@@ -0,0 +1,31 @@
+//<input type="file" accept=".xls, .xlsx" />
+public void Import(IFormFile FormFile)
+{
+  if (FormFile == null || FormFile.Length == 0)
+    //err
+
+  var stream = FormFile.OpenReadStream();
+  IExcelDataReader reader = null;
+  System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+
+  if (FormFile.FileName.EndsWith(".xls"))
+    reader = ExcelReaderFactory.CreateBinaryReader(stream);
+  else if (FormFile.FileName.EndsWith(".xlsx"))
+    reader = ExcelReaderFactory.CreateOpenXmlReader(stream);
+  else
+    //not supported
+
+  var result = reader.AsDataSet();
+  foreach (DataTable Table in result.Tables)
+  {
+    for (int i = 0; i < Table.Rows.Count; i++)
+    {
+      for (int j = 0; j < Table.Columns.Count; j++)
+      {
+        //do sth with Table.Rows[i][j]
+      }
+    }
+  }
+
+  reader.Close();
+}
